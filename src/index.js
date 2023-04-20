385775f7cec8dca7da2e2dba97e8e6fcc3186887
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-04-20 23:24:53 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import refs from './js/refs';
+
+console.log(refs);
+const DEBOUNCE_DELAY = 300;
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  box: document.querySelector('.country-info'),
+};
